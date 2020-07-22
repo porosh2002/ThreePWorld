80406baedb51c5db8368c452d559e361b3d032cf
porosh <jamilkashem2002@gmail.com>
2020-07-22 10:07:50 +0600

    footer disable

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './App.css';
 import Navigation from './Component/Navigation/Navigation'
-import Footer from './Component/Footer/Footer';
+// import Footer from './Component/Footer/Footer';
 function App() {
   return (
     <div>
 <Navigation />
-<Footer />
+{/* <Footer /> */}
     </div>
   );
 }
