8b41b5f2005b6980e9efdfabd91bdbc5598df700
Jamil Kashem Porosh <50365984+porosh2002@users.noreply.github.com>
2020-08-06 23:16:00 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component,Suspense} from "react";
 import { Switch, Route} from "react-router-dom";
 import Loading from "./Component/Loading/Loading";
+import Home from './Pages/Home'
 import Navigation from './Component/Navigation/Navigation'
 import "./App.css";
 export default class App extends Component {
   render() {
-    const Home =React.lazy(() => import('./Pages/Home'));
-    const Error =React.lazy(() => import('./Pages/Error'));
+//     const Home =React.lazy(() => import('./Pages/Home'));
+//     const Error =React.lazy(() => import('./Pages/Error'));
     return (
       <div>
-    <Suspense fallback={<Loading />}>
-      <Navigation />
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route component={Home}/>
-      </Switch>
-    </Suspense>
+      <Home />
+//     <Suspense fallback={<Loading />}>
+//       <Navigation />
+//       <Switch>
+//         <Route exact path="/" component={Home}/>
+//         <Route component={Home}/>
+//       </Switch>
+//     </Suspense>
       </div>
     );
   }
