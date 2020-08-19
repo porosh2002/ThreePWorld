3d043dd88378b0bbb619d2e15afc3ce39c1a5bc2
porosh2002 <jamilkashem2002@gmail.com>
2020-08-20 02:06:56 +0600

    w

--- src/Pages/Product-filter-mob.js
+++ src/Pages/Product-filter-mob.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
 export default class Product_filter_mob extends Component {
+    constructor(){
+        super();
+        this.state = {
+            filterOpen:false,
+        }
+    }
+    menu_div_out = () => {
+        this.setState({
+          filterOpen:true,
+        });
+      };
   render() {
     return (
       <div className='filter-m0b-div'>
         <div className="filter-mob">
-          <span class="fas fa-sliders-h"></span>
+          <div onClick={this.menu_div_out}>
+              <i className="fas fa-sliders-h"></i>
+          </div>
         </div>
       </div>
     );
--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -1,23 +1,25 @@
-import React, { Component, Suspense } from 'react'
-import Loading from '../Component/Loading/Loading'
+import React, { Component, Suspense } from "react";
+import Loading from "../Component/Loading/Loading";
 export default class Product extends Component {
-    render() {
-        const ProductDesk =React.lazy(() => import('../Pages/Product-Filter-desk'));
-        const ProductMob =React.lazy(() => import('../Pages/Product-filter-mob'));
-        return (
-            <div>
+  render() {
+    const ProductDesk = React.lazy(() =>
+      import("../Pages/Product-Filter-desk")
+    );
+    const ProductMob = React.lazy(() => import("../Pages/Product-filter-mob"));
+    return (
+      <div>
         {window.innerWidth > 900 ? (
           <Suspense fallback={<Loading />}>
-            <div className='product-desk'>
-            <ProductDesk />
+            <div className="product-desk">
+              <ProductDesk />
             </div>
           </Suspense>
         ) : (
           <Suspense fallback={<Loading />}>
-<ProductMob />
+            <ProductMob />
           </Suspense>
         )}
-            </div>
-        )
-    }
+      </div>
+    );
+  }
 }
