86de17b14b8af447c102982a9cca1467207b86ab
porosh2002 <jamilkashem2002@gmail.com>
2020-08-19 22:02:19 +0600

    q

--- src/Pages/Product-Filter-desk.js
+++ src/Pages/Product-Filter-desk.js
@@ -3,8 +3,22 @@ import React, { Component } from 'react'
 export default class Product_Filter_desk extends Component {
     render() {
         return (
-        <div>
-            Product Desk
+        <div className='product-filter'>
+            <p className=' title-filter title-main-f'>Filters</p>
+           <p className='title-filter'>CATEGORIES</p>
+           {/* {Catagories} */}
+           <p className='title-filter'>BRAND</p>
+           {/* {brand} */}
+           <p className='title-filter'>PRICE</p>
+           {/* {price} */}
+           <p className='title-filter'>COLOR</p>
+
+           <p className='title-filter'>SIZE</p>
+           {/* {Size} */}
+           <p className='title-filter'>STORE BY</p>
+           {/* {Story BY} */}
+           <p className='title-filter'>DISCOUNT RANGE</p>
+           {/* {offer} */}
         </div>
         )
     }
