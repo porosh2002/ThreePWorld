5a146bd7d7b2b5308d46fea246f013488cc6aea5
porosh2002 <jamilkashem2002@gmail.com>
2020-08-26 19:32:12 +0600

    a

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export default function Login() {
     return (
         <div>
             <form>
                 <div className='form-login'>
-                    <p className='title-login'>Login</p>
+                <p className='title-login'>Login</p>
                 <input className='email-input' type='email' placeholder='e.g. example@example.com' />
                 <input className='password-input' type='password' placeholder='Enter Your Password Here' />
                 <input className='submit-input' type='submit' value='Login'/>
+                <Link className='undr-form' to='' >Create New Account</Link>
+                <Link className='undr-form' to='' >Forgot Password</Link>
                 </div>
             </form>
         </div>
