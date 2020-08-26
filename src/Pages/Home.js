89659d188d322384669d34705f1aa3274946d346
porosh2002 <jamilkashem2002@gmail.com>
2020-08-26 20:46:21 +0600

    ops

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import Slider from '../Component/Slider/Slider'
-import SliderOffer from '../Component/Slider/offerSlider'
+// import Slider from '../Component/Slider/Slider'
+// import SliderOffer from '../Component/Slider/offerSlider'
 export default function Home() {
     return (
         <div>
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,21 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-export default function Login() {
-    return (
-        <div>
+export default class Signup extends Component {
+    Submit=()=>{
+        }
+    render() {
+        return (
+            <div>
             <form>
                 <div className='form-login'>
                 <p className='title-login'>Sign UP</p>
-                <input className='password-input' type='password' placeholder='Enter Your Name :' />
+                <input className='password-input' type='name' placeholder='Enter Your Name :' />
                 <input className='email-input' type='email' placeholder='Enter Your Email :' />
                 <input className='password-input' type='password' placeholder='Enter New Password Here' />
                 <input className='password-input' type='password' placeholder='Re-Enter New Password Here' />
+                <input className='password-input' type='password' placeholder='Enter Your Address' />
+                <input className='password-input' type='password' placeholder='Enter Your district' />
                 <input className='password-input' type='password' placeholder='Referral Number (Optional)' />
-                <input className='submit-input' type='submit' value='Login'/>
+                <input className='submit-input' onClick={this.Submit()} type='submit' value='Submit'/>
                 <Link className='undr-form' to='/Login' >Log In</Link>
                 <Link className='undr-form' to='' >Forgot Password</Link>
                 </div>
             </form>
         </div>
-    )
+        )
+    }
 }
