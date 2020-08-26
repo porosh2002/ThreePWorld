e7c013c0c947c3786fced5e5b583ba9814c48e31
porosh2002 <jamilkashem2002@gmail.com>
2020-08-26 15:37:33 +0600

    a

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -2,8 +2,15 @@ import React from 'react'
 
 export default function Login() {
     return (
-        <div className='Login'>
-            
+        <div>
+            <form>
+                <div className='form-login'>
+                    <p className='title-login'>Login</p>
+                <input className='email-input' type='email' placeholder='e.g. example@example.com' />
+                <input className='password-input' type='password' placeholder='Enter Your Password Here' />
+                <input className='submit-input' type='submit' value='Login'/>
+                </div>
+            </form>
         </div>
     )
 }
