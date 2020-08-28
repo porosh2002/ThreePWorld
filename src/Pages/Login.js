479a1a38dd57ab77b249d89a06405fd1f3d3b10d
porosh2002 <jamilkashem2002@gmail.com>
2020-08-28 09:56:49 +0600

    a

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,18 +1,46 @@
-import React from 'react'
+import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
-export default function Login() {
-    return (
-        <div>
+export default class Login extends Component {
+    constructor() {
+        super()
+        this.state = {
+          email:'',
+          password:''
+        }
+      }
+      onEmailChange=(event)=>{
+          this.setState({email:event.target.value})
+      }
+      onPasswordChange=(event)=>{
+          this.setState({password:event.target.value})
+      }
+    onSubmit=()=>{
+if(this.state.email.length>6 || this.state.password>5){
+    fetch('http://localhost:3000/Login',{
+        method:'post',
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify({
+            email:this.state.email,
+            password:this.state.password
+        })
+    })
+}
+    }
+    render() {
+        return (
+            <div>
             <form>
                 <div className='form-login'>
                 <p className='title-login'>Login</p>
-                <input className='email-input' type='email' placeholder='e.g. example@example.com' />
-                <input className='password-input' type='password' placeholder='Enter Your Password Here' />
-                <input className='submit-input' type='submit' value='Login'/>
+                <input className='email-input' required onChange={this.onEmailChange} type='email' placeholder='e.g. example@example.com' />
+                <input className='password-input' required minLength='6' onChange={this.onPasswordChange} type='password' placeholder='Enter Your Password Here' />
+                <input className='submit-input' onClick={this.onSubmit} type='submit' value='Login'/>
                 <Link className='undr-form' to='/Register' >Create New Account</Link>
                 <Link className='undr-form' to='' >Forgot Password</Link>
                 </div>
             </form>
-        </div>
-    )
+            </div>
+        )
+    }
 }
+
--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,27 +1,65 @@
 import React, { Component } from 'react'
 import {Link} from 'react-router-dom'
 export default class Signup extends Component {
-    Submit=()=>{
+    constructor() {
+        super()
+        this.state = {
+          name:'',
+          email:'',
+          password:'',
+          Address:'',
+          district:'',
+          Refferal:''
         }
+      }
+      onNameChange=(event)=>{
+          this.setState({name:event.target.value})
+      }
+      onEmailChange=(event)=>{
+          this.setState({email:event.target.value})
+      }
+      onPasswordChange=(event)=>{
+          this.setState({password:event.target.value})
+      }
+      onAddressChange=(event)=>{
+          this.setState({Address:event.target.value})
+      }
+      onDistrictChange=(event)=>{
+          this.setState({district:event.target.value})
+      }
+      onRefferalChange=(event)=>{
+          this.setState({Refferal:event.target.value})
+      }
+    onSubmit=()=>{
+if(this.state.email.length>6 || this.state.password>5){
+    fetch('http://localhost:3000/Register',{
+        method:'post',
+        headers: {'Content-Type': 'application/json'},
+        body:JSON.stringify({
+            email:this.state.email,
+            password:this.state.password
+        })
+    })
+}
+    }
     render() {
         return (
             <div>
-            <form>
+                <form>
                 <div className='form-login'>
                 <p className='title-login'>Sign UP</p>
-                <input className='password-input' type='name' placeholder='Enter Your Name :' />
-                <input className='email-input' type='email' placeholder='Enter Your Email :' />
-                <input className='password-input' type='password' placeholder='Enter New Password Here' />
-                <input className='password-input' type='password' placeholder='Re-Enter New Password Here' />
-                <input className='password-input' type='password' placeholder='Enter Your Address' />
-                <input className='password-input' type='password' placeholder='Enter Your district' />
-                <input className='password-input' type='password' placeholder='Referral Number (Optional)' />
-                <input className='submit-input' onClick={this.Submit()} type='submit' value='Submit'/>
+                <input className='password-input' required onChange={this.onNameChange} type='name' placeholder='Enter Your Name :' />
+                <input className='email-input' required onChange={this.onEmailChange} minLength='6' type='email' placeholder='Enter Your Email :' />
+                <input className='password-input' required onChange={this.onPasswordChange} minLength='6' type='password' placeholder='Enter New Password Here' />
+                <input className='password-input' required onChange={this.onAddressChange} minLength='7' type='' placeholder='Enter Your Address' />
+                <input className='password-input' required onChange={this.onDistrictChange} type='text' placeholder='Enter Your district' />
+                <input className='password-input' onChange={this.onRefferalChange} type='password' minLength='6' placeholder='Referral Number (Optional)' />
+                <input className='submit-input' type='submit' value='Submit'/>
                 <Link className='undr-form' to='/Login' >Log In</Link>
                 <Link className='undr-form' to='' >Forgot Password</Link>
                 </div>
             </form>
-        </div>
+            </div>
         )
     }
 }
