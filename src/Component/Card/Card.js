b6bf96052d06c63c2a614a04e4a8e6b883e91568
porosh2002 <jamilkashem2002@gmail.com>
2020-09-06 10:15:24 +0600

    a

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -2,29 +2,28 @@ import React, { Component,Suspense } from 'react'
 
 export default class Card extends Component {
   componentDidMount(){
-    console.log('Did render');
      fetch('http://localhost:5000/geTiMagE/kelfzq6b')
-      .then(response=> response.json())
       .then(users => { this.setState({ image: users})});
   }
   constructor(props){
     super(props);
     this.state={
-      image:[]
+      image:[],
     }
   }
   render() {
-    // const {name,price,offer,imageID} = this.props
-    // console.log(this.state.image);
-    console.log('Card Component');
-    console.log(this.state.image);
+    console.log(this.state.image.url);
+    const {name,price,offer,imageID} = this.props;
+    // if(this.state.image.length>0){
+    //   this.setState({imageURL:'data:image/;base64' + this.state.image[0].image1.data})
+    // }
     return (
 <div>
-{/* {this.state.image.length > 0 ? (
+{this.state.image.url !== null ? (
         <Suspense fallback={<p>...</p>}>
     <div className='card'>
-      
       <div>
+        <img src={this.state.image.url} />
         <h3>{name}</h3>
         <p>{price}</p>
         <p>{offer}</p>
@@ -36,7 +35,7 @@ export default class Card extends Component {
         <Suspense fallback={<p>...</p>}>
           <div></div>
         </Suspense>
-      )} */}
+      )}
 </div>
 
     )
