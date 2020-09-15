3cd7ae03de6f95ec498acfeeadbc38ced48a9390
porosh2002 <jamilkashem2002@gmail.com>
2020-09-15 19:24:42 +0600

    a

--- src/Component/DsbrdUSR/DU.js
+++ src/Component/DsbrdUSR/DU.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function DU() {
-    return (
-        <div className='dsbrd'>
-<div className='links-dsbrd'>
-<Link className='dsLnk' to=''>Profile</Link>
-            <Link className='dsLnk' to=''>Earn</Link>
-            <Link className='dsLnk' to='/Login'>Logout</Link>
-</div>
-        </div>
-    )
+  return (
+    <div className="dsbrd">
+      <div className="links-dsbrd">
+        <Link className="dsLnk" to="">
+          Profile
+        </Link>
+        <Link className="dsLnk" to="">
+          Earn
+        </Link>
+        <Link className="dsLnk" to="/Login">
+          Logout
+        </Link>
+      </div>
+    </div>
+  );
 }
