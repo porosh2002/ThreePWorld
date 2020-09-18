3a877610844760e5cf3d04972e20c6519c59a85d
porosh2002 <jamilkashem2002@gmail.com>
2020-09-19 00:03:57 +0600

    with axiox

--- src/Component/Feature/Feature.js
+++ src/Component/Feature/Feature.js
@@ -1,25 +1,24 @@
-import React, { Component,Suspense} from 'react'
-import CardList from '../CardList/CardList'
+import React, { Component } from "react";
+const axios = require("axios");
+import CardList from "../CardList/CardList";
 export default class Feature extends Component {
-    componentDidMount() {
-        fetch("http://localhost:5000/AllProduct")
-          .then((response) => response.json())
-          .then((users) => {
-            this.setState({ robots: users });
-          });
-      }
-      constructor(){
-          super();
-          this.state={
-              robots:[]
-          }
-      }
-    render() {
-        return (
-            <div className='fpd'>
-            <h2 className='fp'>Feature Products</h2>     
-            <CardList  robots={this.state.robots}/>
-            </div>
-        )
-    }
+  componentDidMount() {
+    axios.get("http://localhost:5000/AllProduct").then((data) => {
+      this.setState({ robots: data.data });
+    });
+  }
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+    };
+  }
+  render() {
+    return (
+      <div className="fpd">
+        <h2 className="fp">Feature Products</h2>
+        <CardList robots={this.state.robots} />
+      </div>
+    );
+  }
 }
