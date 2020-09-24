2e554a0c26fb0fe58a067b8242ae87a37c833dfa
porosh2002 <jamilkashem2002@gmail.com>
2020-09-24 23:29:05 +0600

    a

--- src/Pages/Vendorpost.js
+++ src/Pages/Vendorpost.js
@@ -1,5 +1,6 @@
 import React, { Component,Suspense} from 'react'
 import DU2 from '../Component/DsbrdUSR/DU2'
+import {Link} from 'react-router-dom'
 import uniqid from 'uniqid'
 export default class User extends Component {
     componentWillMount() {
@@ -28,10 +29,32 @@ constructor(){
       BrandName: "",
       imageID:"",
       NidImage:"",
-      NidUserID:""
+      NidUserID:"",
+      addProduct: true,
+      deleteProduct: false,
+      editProduct: false,
+      deleteId:'',
+      vendorEdit: "",
+      iteamEdit: "",
+      priceEdit: "",
+      descriptionEdit: "",
+      tagsEdit: "",
+      sizeEdit: "",
+      offerEdit: "",
+      BrandNameEdit: "",
+      imageIDEdit: "",
     } 
 }
-
+onpdctedit=event=>{
+  fetch(`http://localhost:5000/Product/${event.target.value}`)
+  .then((response) => response.json())
+  .then((users) => {
+    this.setState({imageID:users[0].imageID,vendorEdit:users[0].vendor,iteamEdit:users[0].iteam,priceEdit:users[0].price,BrandNameEdit:users[0].BrandName,descriptionEdit:users[0].description,tagsEdit:users[0].tags,sizeEdit:users[0].size,offerEdit:users[0].offer,imageIDEdit:users[0].imageID})
+  });
+}
+ondeleteid=e=>{
+  this.setState({deleteId:e.target.value})
+}
 fileChangeNID=event=>{
     this.setState({ NidImage: event.target.files[0]});
   }
@@ -43,6 +66,39 @@ const formData = new FormData();
     method: "POST",
     body:formData,
   });
+  }
+  onbrandnameupdate=e=>{
+    this.setState({BrandNameEdit:e.target.value})
+  }
+  onoffrupdate=e=>{
+    this.setState({offerEdit:e.target.value})
+  }
+  onsizeupdate=e=>{
+    this.setState({sizeEdit:e.target.value})
+  }
+  ontagsupdate=e=>{
+    this.setState({tagsEdit:e.target.value})
+  }
+  ondesupdate=e=>{
+    this.setState({descriptionEdit:e.target.value})
+  }
+  onpriceupdate=e=>{
+    this.setState({priceEdit:e.target.value})
+  }
+  onvendorupdate=e=>{
+    this.setState({vendorEdit:e.target.value})
+  }
+onnameupdate=e=>{
+  this.setState({iteamEdit:e.target.value})
+}
+  activateAddproduct=()=>{
+    this.setState({addProduct:true,editProduct:false,deleteProduct:false})
+  }
+  activateEditproduct=()=>{
+    this.setState({addProduct:false,editProduct:true,deleteProduct:false})
+  }
+  activateDeleteproduct=()=>{
+    this.setState({addProduct:false,editProduct:false,deleteProduct:true})
   }
 onBRANDChange=event=>{
     this.setState({ BrandName: event.target.value});
@@ -104,11 +160,41 @@ onBRANDChange=event=>{
       })
     })
   };
+  updateproduct=()=>{
+    fetch(`http://localhost:5000/ProductEdit/${this.state.imageID}`, {
+      method: "post",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        iteam: this.state.iteamEdit,
+        price: this.state.priceEdit,
+        vendor: this.state.vendorEdit,
+        description: this.state.descriptionEdit,
+        tags: this.state.tagsEdit,
+        size: this.state.sizeEdit,
+        offer: this.state.offerEdit,
+        BrandName: this.state.BrandNameEdit
+      }),
+    });
+  }
+  ondeleteProduct=()=>{
+    fetch(`http://localhost:5000/deleteProduct/${this.state.deleteId}`,{
+      method:"post"
+    })
+  }
     render() {
-        const {displayproduct} = this.state;
-        const styleproduct = {display:displayproduct};
+      const {addProduct} = this.state;
+      const {deleteProduct} = this.state;
+      const {editProduct} = this.state;
+      const style = addProduct ? { display: "block" } : { display: "none" };
+      const style2 = deleteProduct ? { display: "block" } : { display: "none" };
+      const style3 = editProduct ? { display: "block" } : { display: "none" };
         return (
             <div>
+              <div className='opsLink'>
+      <Link onClick={this.activateAddproduct} className='opsLink_' >Add Product</Link>
+      <Link onClick={this.activateDeleteproduct} className='opsLink_' >Edit Product</Link>
+      <Link onClick={this.activateEditproduct} className='opsLink_' >Delete Product</Link>
+    </div>
             <DU2 id={this.props.match.params.id}/>
             <div  className='dsbrd-cntnt'>
                 <p className='title'>Profile :</p>
@@ -117,12 +203,12 @@ onBRANDChange=event=>{
                 
                   {this.state.access ? (
           <Suspense fallback={<div>Loading...</div>}>
-          <p className='vndr-right-ok'>You Have Rights to Add Product </p>
-          </Suspense>
+          <p className='vndr-right-ok'>You Have Rights to Add,Delete,Edit Product </p>
+          </Suspense> 
         ) : (
           <Suspense fallback={<div>Loading...</div>}>
           <div>
-          <p className='vndr-right-warn'>You Have Rights to Add Product </p>
+          <p className='vndr-right-warn'>You Have No Rights to Add,Delete,Edit Product </p>
           <p className='vndr-right-warn'>Submit Document to get access</p>
           <input
               className="img-admin-input"
@@ -139,7 +225,7 @@ onBRANDChange=event=>{
             </div> 
             <div className="main-admin">
         <div className="main-admin-content">
-          <div style={styleproduct} className="addProduct">
+          <div style={style} className="addProduct">
             <p className='title-admin'>Add Product by Vendor</p>
             <input onChange={this.oniteamChange}
               className="admin-input"
@@ -198,6 +284,70 @@ onBRANDChange=event=>{
               ADD Product
             </button>
           </div>
+          <div style={style2} className="GetProductforEdit">
+          <p className="title-admin">Edit Product</p>
+          <input onChange={this.onpdctedit} className="admin-input" type="text" required></input>
+          <p  className="edit-pdct-dtls">Name: {this.state.iteamEdit}</p>
+          <p  className="edit-pdct-dtls">Price: {this.state.priceEdit}</p>
+          <p  className="edit-pdct-dtls">Vendor: {this.state.vendorEdit}</p>
+          <p  className="edit-pdct-dtls">
+            Description: {this.state.descriptionEdit}
+          </p>
+          <p  className="edit-pdct-dtls">Tag's: {this.state.tagsEdit}</p>
+          <p  className="edit-pdct-dtls">Size: {this.state.sizeEdit}</p>
+          <p  className="edit-pdct-dtls">Offer: {this.state.offerEdit}</p>
+          <p  className="edit-pdct-dtls">
+            BrandName: {this.state.BrandNameEdit}
+          </p>
+          <input onChange={this.onnameupdate}
+            className="admin-input"
+            type="text"
+            placeholder="Product  Name"
+          ></input>
+          <input onChange={this.onpriceupdate}
+            className="admin-input"
+            type="text"
+            placeholder="Product  price"
+          ></input>
+          <input onChange={this.onsizeupdate}
+            className="admin-input"
+            type="text"
+            placeholder="Product  Size"
+          ></input>
+          <input onChange={this.onoffrupdate}
+            className="admin-input"
+            type="text"
+            placeholder="Product  offer"
+          ></input>
+          <input onChange={this.onbrandnameupdate}
+            className="admin-input"
+            type="text"
+            placeholder="Product  Brand"
+          ></input>
+          <input onChange={this.onvendorupdate}
+            className="admin-input"
+            type="text"
+            placeholder="Vendor Name"
+          ></input>
+          <textarea onChange={this.ondesupdate}
+            className="admin-txtarea"
+            placeholder="Enter Product Description"
+          />
+          <textarea onChange={this.ontagsupdate}
+            className="admin-txtarea"
+            placeholder="Enter Product Tags"
+          />
+          <button onClick={this.updateproduct} className="btn-upld-pdtc">
+              Update Product
+            </button>
+        </div>
+        <div style={style3} className="GetProductforEdit">
+          <p className="title-admin">Delete Product</p>
+          <input onChange={this.ondeleteid} className="admin-input" type="text" required></input>
+          <button onClick={this.ondeleteProduct} className="btn-upld-pdtc">
+              Delete Product
+            </button>
+        </div>
         </div>
       </div>
         </div>
